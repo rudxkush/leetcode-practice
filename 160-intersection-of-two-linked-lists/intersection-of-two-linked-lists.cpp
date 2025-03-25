b50f4cf02ab3c8e05d515cecb888d3d97c596df5
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-03-25 16:39:49 +0530

    Time: 41 ms (36.25%) | Memory: 18.6 MB (53.86%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -9,33 +9,44 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* iterA = headA;
+        ListNode* iterA = headA;    
         ListNode* iterB = headB;
-        int lenA = 0, lenB = 0;
-        while (iterA != NULL) { lenA++; iterA = iterA->next; }
-        while (iterB != NULL) { lenB++; iterB = iterB->next; }
-        
-        // Reset iterators
-        iterA = headA;
-        iterB = headB;
-        
-        // Adjust starting points to align the lists
-        if (lenA > lenB) {
-            for (int i = 0; i < lenA - lenB; i++) {
-                iterA = iterA->next;
-            }
-        } else {
-            for (int i = 0; i < lenB - lenA; i++) {
-                iterB = iterB->next;
-            }
+        while(iterA != iterB){
+            if (iterA == NULL) iterA = headB;
+            else iterA = iterA->next;
+
+            if (iterB == NULL) iterB = headA;
+            else iterB = iterB->next;
         }
-        while(iterA != nullptr && iterB != nullptr) {
-            if(iterA == iterB) {
-                return iterA;
-            }
-            iterA = iterA->next;
-            iterB = iterB->next; 
-        }
-        return nullptr;
+        return iterA;
+
+        // ListNode* iterA = headA;
+        // ListNode* iterB = headB;
+        // int lenA = 0, lenB = 0;
+        // while (iterA != NULL) { lenA++; iterA = iterA->next; }
+        // while (iterB != NULL) { lenB++; iterB = iterB->next; }
+        
+        // // Reset iterators
+        // iterA = headA;
+        // iterB = headB;
+        
+        // // Adjust starting points to align the lists
+        // if (lenA > lenB) {
+        //     for (int i = 0; i < lenA - lenB; i++) {
+        //         iterA = iterA->next;
+        //     }
+        // } else {
+        //     for (int i = 0; i < lenB - lenA; i++) {
+        //         iterB = iterB->next;
+        //     }
+        // }
+        // while(iterA != nullptr && iterB != nullptr) {
+        //     if(iterA == iterB) {
+        //         return iterA;
+        //     }
+        //     iterA = iterA->next;
+        //     iterB = iterB->next; 
+        // }
+        // return nullptr;
     }
 };
