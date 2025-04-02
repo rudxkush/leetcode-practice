38bfaec719d833e16437e0245154fd2aa59f5d74
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-04-02 18:55:53 +0530

    Time: 6 ms (96.80%) | Memory: 60.6 MB (77.60%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.cpp
@@ -1,17 +1,36 @@
+// class Solution {
+// public:
+//     vector<int> smallestSubarrays(vector<int>& A) {
+//         int last[30] = {}, n = A.size();
+//         vector<int> res(n, 1);
+//         for (int i = n - 1; i >= 0; --i) {
+//             for (int j = 0; j < 30; ++j) {
+//                 if (A[i] & (1 << j))
+//                     last[j] = i;
+//                 res[i] = max(res[i], last[j] - i + 1);
+//             }
+//         }
+//         return res;
+//     }
+// };
 class Solution {
-public:
-    vector<int> smallestSubarrays(vector<int>& A) {
-        int last[30] = {}, n = A.size();
-        vector<int> res(n, 1);
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = 0; j < 30; ++j) {
-                if (A[i] & (1 << j))
-                    last[j] = i;
-                res[i] = max(res[i], last[j] - i + 1);
-            }
-        }
-        return res;
-    }
+   public:
+	static vector<int> smallestSubarrays(vector<int>& nums) {
+		const size_t n = nums.size();
+		vector<int> ans(n);
+
+		for (int i = 0; i < n; i++) {
+			ans[i] = 1;
+			for (int j = i - 1; j >= 0; j--) {
+				if ((nums[j] | nums[i]) == nums[j]) break;
+
+				nums[j] |= nums[i];
+				ans[j] = i - j + 1;
+			}
+		}
+
+		return ans;
+	}
 };
 
 // vector<int> smallestSubarrays(vector<int>& nums) {
