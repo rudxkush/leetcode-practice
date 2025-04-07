830971c1230a927a0711a7fa2dc507e875abc9c2
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-04-08 02:01:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (60.65%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,13 +1,32 @@
-class Solution {
+class Solution { 
 public:
+    int m, n;
     int kthSmallest(vector<vector<int>>& matrix, int k) {
-        priority_queue<int> maxHeap; 
-        for(int i = 0; i < matrix.size(); i++) {
-            for(int j = 0; j < matrix[i].size(); j++) {
-                maxHeap.push(matrix[i][j]);
-                if(maxHeap.size() > k) maxHeap.pop();
-            }
+        m = matrix.size(),
+        n = matrix[0].size(); 
+        int left = matrix[0][0], right = matrix[m - 1][n - 1], ans = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (countLessOrEqual(matrix, mid) >= k) {
+                ans = mid;
+                right =
+                    mid - 1; 
+            } else
+                left = mid + 1; 
         }
-        return maxHeap.top();
+        return ans;
+    }
+    // similar to search in a 2D sorted matrix" problem where you start from the
+    // top-right corner and move toward the bottom-left corner to find a
+    // specific target.
+    int countLessOrEqual(vector<vector<int>>& matrix, int x) {
+        int count = 0, col = n - 1;
+        for (int row = 0; row < m; row++) {
+            while (col >= 0 && matrix[row][col] > x){
+                --col;
+            }
+            count += (col + 1);
+        }
+        return count;
     }
 };
