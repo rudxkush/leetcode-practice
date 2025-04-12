861a51d300cb594f49b9ca3c4fb0be64bf60cb15
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-04-12 14:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (94.61%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-  int dfs(TreeNode* node, int &maxSum){
-    if(!node) return 0;
+    int dfs(TreeNode* node, int& maxSum) {
+        if (!node)
+            return 0;
 
-    // used max to reset any path with negative sum
-    int leftSum = max(0, dfs(node->left, maxSum));
-    int rightSum = max(0, dfs(node->right, maxSum));
+        int leftSum = max(0, dfs(node->left, maxSum));
+        int rightSum = max(0, dfs(node->right, maxSum));
 
-    maxSum = max(maxSum, leftSum+rightSum+node->val);
+        maxSum = max(maxSum, leftSum + rightSum + node->val);
+        
+        return max(leftSum, rightSum) + node->val;
+    }
 
-    // since you need to choose a path either left or right
-    return max(leftSum, rightSum) + node->val;
-  }
-
-  int maxPathSum(TreeNode* root) {
-    int maxSum = root->val;
-    dfs(root, maxSum);
-    return maxSum;
-  }
+    int maxPathSum(TreeNode* root) {
+        int maxSum = root->val;
+        dfs(root, maxSum);
+        return maxSum;
+    }
 };
