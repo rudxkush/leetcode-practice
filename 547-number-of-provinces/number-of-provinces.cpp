1a27e876eca03062377cf570393e23fd60498190
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-03 06:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (63.13%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,21 +1,60 @@
-class Solution {
+class DisjointSet {
 public:
-    void dfs(vector<vector<int>>& isConnected, int srcNode, vector<bool>& visited) {
-        visited[srcNode] = true;
-        for (int i = 0; i < isConnected.size(); i++) {
-            if (isConnected[srcNode][i] && !visited[i]) {
-                dfs(isConnected, i, visited);
-            }
+    vector<int> parent, size;
+    // Initializes disjoint sets
+    DisjointSet(int n) {
+        parent.resize(n+1);
+        size.resize(n+1, 1);
+        for(int i = 0;i <= n; i++) {
+            parent[i] = i;
         }
     }
+    // path compression
+    int findUltimateParent(int node) {
+        if(node == parent[node])
+            return node;
+            
+        return parent[node] = findUltimateParent(parent[node]);
+    }
+    
+    void unionBySize(int u, int v) {
+     int ultimateParent_u = findUltimateParent(u);
+     int ultimateParent_v = findUltimateParent(v);
+      
+     if(ultimateParent_u == ultimateParent_v) return ;
+      
+     if(size[ultimateParent_u] < size[ultimateParent_v]) {
+       parent[ultimateParent_u] = ultimateParent_v; 
+       size[ultimateParent_v] += size[ultimateParent_u];
+     } else {
+       parent[ultimateParent_v] = ultimateParent_u; 
+       size[ultimateParent_u] += size[ultimateParent_v];
+     }
+   }
+};
+class Solution {
+public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        vector<bool> visited(n);
+        int V = isConnected.size();
+        
+        vector<int> parent(V + 1), size(V + 1, 1); 
+        for(int i = 0; i <= V; i++) {
+            parent[i] = i;
+        }
+
+        DisjointSet ds(V);
+        for(int i = 0; i < V; i++) {
+            for(int j = 0; j < V; j++) {
+                if(isConnected[i][j] == 1) {
+                    ds.unionBySize(i + 1, j + 1);
+                }
+            }
+        }
+
         int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+        for(int i = 1; i <= V; i++) {
+            if(ds.parent[i] == i) {
                 count++;
-                dfs(isConnected, i, visited);
             }
         }
         return count;
