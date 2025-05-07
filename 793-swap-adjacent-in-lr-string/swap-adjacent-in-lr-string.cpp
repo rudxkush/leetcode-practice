2361f128e405583294cf5aa36e0e10d393bb738a
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-07 06:56:48 +0530

    Time: 4 ms (24.12%) | Memory: 9.8 MB (66.96%) - LeetSync

--- 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.cpp
+++ 793-swap-adjacent-in-lr-string/swap-adjacent-in-lr-string.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     bool canTransform(string start, string end) {
+        // instead of trying to prove it 
+        // rather look for where it fails
         int i = 0;
         int j = 0;
-        int n = start.size();
-        int m = end.size();
+        int n = start.size(), m = end.size();
         if(n != m) return false;
-
         while(i < n || j < n) {
             while(start[i] == 'X') i++;
             while(end[j] == 'X') j++;
 
             if(start[i] != end[j]) return false;
-            if(start[i] == 'R' && j < i) return false;
-            if(start[i] == 'L' && j > i) return false;
+            if(start[i] == 'R' && i > j) return false;
+            if(start[i] == 'L' && i < j) return false;
             i++, j++;
         }
         return true;
