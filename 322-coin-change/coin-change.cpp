05b1f10ec2f4d1e766e7f9586827530f46ec94a5
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-12 10:43:09 +0530

    Time: 23 ms (68.31%) | Memory: 17.9 MB (69.73%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int unboundedKnapsack(vector<int>& coins, int i, int amount) {
-        // base cases
-        if(amount == 0) return 0;
-        if(i < 0 || amount < 0) return INT_MAX;
-
-        if(dp[i][amount] != -1) return dp[i][amount];
-
-        // Include current coin
-        int include = unboundedKnapsack(coins, i, amount - coins[i]);
-        if (include != INT_MAX) include += 1;
-
-        // Exclude current coin
-        int exclude = unboundedKnapsack(coins, i - 1, amount);
-
-        return dp[i][amount] = min(include, exclude);
-    }
-
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        dp.assign(n, vector<int>(amount + 1, -1));
-        int answer = unboundedKnapsack(coins, n - 1, amount);
-        return answer == INT_MAX ? -1 : answer;
+        vector<int> dp(amount + 1, amount + 1); 
+        dp[0] = 0;
+
+        for(int i = 1; i <= amount; i++) {
+            for(int j = 0; j < n; j++) {
+                if(coins[j] <= i) {
+                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
