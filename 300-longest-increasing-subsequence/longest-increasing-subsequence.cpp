733cb07fa494da05499f9e4a43e12881ef17044a
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-14 09:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.22%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,15 @@
-class Solution { // bottom up 
+class Solution {
 public:
-    vector<vector<int>> dp;
-    int knapsack(vector<int>& nums, int i, int prev) {
-        // base case
-        if(i >= nums.size()) {
-            return 0;
-        }
-
-        if(dp[i][prev + 1] != -1) return dp[i][prev + 1];
-
-        // inclusion
-        int include = 0; 
-        if(prev == -1 || nums[i] > nums[prev])
-            include = 1 + knapsack(nums, i + 1, i);
-
-        // exclusion
-        int exclude = knapsack(nums, i + 1, prev);
-
-        return dp[i][prev + 1] = max(include, exclude);  
-    } 
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        dp.resize(n, vector<int> (n + 1, -1));
-        return knapsack(nums, 0, -1);
+        vector<int> res;
+        for(int num : nums) {
+            if(res.empty() || res.back() < num) {
+                res.push_back(num);
+            } else {
+                auto it = lower_bound(res.begin(), res.end(), num);
+                *it = num;
+            }
+        }
+        return res.size();
     }
 };
