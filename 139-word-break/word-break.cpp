ac2d122a63db86e94cf59ee2306637fb50131cbe
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-15 15:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (58.12%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,27 +1,81 @@
+class Node {
+public:
+    // Every index corresponding to the lower case letter
+    // has a reference ptr to another node
+    Node* links[26];
+    bool flag = false;
+    bool containKeys(char ch) { return (links[ch - 'a'] != NULL); }
+    // putting ptr to node char list to another newly created node
+    void put(char ch, Node* node) { links[ch - 'a'] = node; }
+    Node* get(char ch) { return links[ch - 'a']; }
+    void setEnd() { flag = true; }
+    bool isEnd() { return flag; }
+};
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() { root = new Node(); }
+    void insert(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containKeys(word[i])) {
+                node->put(word[i], new Node());
+            }
+            // move to the reference trie
+            node = node->get(word[i]);
+        }
+        node->setEnd();
+    }
+    bool search(string word) {
+        Node* node = root;
+        for (int i = 0; i < word.length(); i++) {
+            if (!node->containKeys(word[i])) {
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->isEnd();
+    }
+    Node* getRoot() { return root; }
+    bool checkWordFromIndex(const string& s, int start, vector<int>& dp) {
+        Node* node = root;
+        for (int i = start; i < s.size(); ++i) {
+            if (!node->containKeys(s[i]))
+                return false;
+            node = node->get(s[i]);
+            if (node->isEnd() && dp[i + 1])
+                return true;
+        }
+        return false;
+    }
+};
+
 class Solution {
 public:
-    unordered_map<int, bool> memo;
-    bool fn(string& s, vector<string>& wordDict, int i) {
-        // base case
-        if(i == s.size()) {
-            return true;
-        }
-        if (memo.find(i) != memo.end()) {
-            return memo[i];
-        }
-        // choices
-        for(int j = 0; j < wordDict.size(); j++) {
-            if(s.substr(i, wordDict[j].size()) != wordDict[j]) { // validating choice 
-                continue;
-            }
-            if (fn(s, wordDict, i + wordDict[j].size())) { // move forward ->
-                return memo[i] = true;
-            }
-        }
-        return memo[i] = false; // backtrack
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        string ans;
-        return fn(s, wordDict, 0);
+        Trie trie;
+        for (string& word : wordDict) {
+            trie.insert(word);
+        }
+
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1; 
+
+        for (int i = n - 1; i >= 0; i--) {
+            Node* node = trie.getRoot(); 
+            for (int j = i; j < n; ++j) {
+                if (!node->containKeys(s[j]))
+                    break;
+                node = node->get(s[j]);
+                if (node->isEnd() && dp[j + 1]) {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
     }
 };
