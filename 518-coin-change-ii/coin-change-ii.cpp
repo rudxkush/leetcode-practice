17bc090482749f04193eebd59ee9302053ac7b71
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-18 22:24:44 +0530

    Time: 19 ms (55.78%) | Memory: 28.5 MB (39.87%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int unboundedKnapsack(vector<int>& arr, int target, int i) {
+    vector<vector<int>> memo;
+    // no of ways we could form the amount using combinations on coins
+    int cHelper(int amount, vector<int>& coins, int i) {
         // base case
-        if (i < 0) {
-            if (target == 0) {
-                return 1;
-            }
+        if(amount == 0) return 1;
+        if(i < 0) 
             return 0;
-        }
-        // if target went beyond 0
-        if (target < 0) {
-            return 0;
-        }
-        // if already computed!
-        if (dp[i][target] != -1) {
-            return dp[i][target];
-        }
-        // choice : include
-        int include = 0;
-        if (arr[i] <= target) {
-            include = unboundedKnapsack(arr, target - arr[i], i);
-        }
-        // choice : exclude
-        int exclude = unboundedKnapsack(arr, target, i - 1);
 
-        return dp[i][target] = (include + exclude);
-    }
-    int change(int target, vector<int>& arr) {
-        int n = arr.size();
-        dp.assign(n, vector<int>(target + 1, -1));
-        return unboundedKnapsack(arr, target, n - 1);
+        if(memo[i][amount] != -1)
+            return memo[i][amount];
+
+        int include = 0;
+        if(coins[i] <= amount)
+            include += cHelper(amount - coins[i], coins, i);
+
+        int exclude = 0;
+            exclude += cHelper(amount, coins, i - 1);
+
+        return memo[i][amount] = (include + exclude);
+
+    } 
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        memo.resize(n, vector<int> (amount + 1, -1));
+        return cHelper(amount, coins, n - 1);
     }
 };
