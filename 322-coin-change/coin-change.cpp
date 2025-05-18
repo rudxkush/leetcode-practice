b89316c27074e3b9b4918149a0cdd91e3521858c
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-18 14:59:24 +0530

    Time: 63 ms (12.88%) | Memory: 49.8 MB (19.94%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int coinChangeHelper(vector<int>& coins, int i, int amount) {
+        // base case
+        if(amount == 0) return 0;
+        if(i < 0 || amount < 0) return 1e9;
+
+        if(dp[i][amount] != -1) return dp[i][amount];
+
+        int taken = 1e9;
+        if(coins[i] <= amount)
+            taken = 1 +  coinChangeHelper(coins, i, amount - coins[i]);
+
+        int notTaken = coinChangeHelper(coins, i - 1, amount);
+
+        return dp[i][amount] = min(taken, notTaken);
+    }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<int> dp(amount + 1, amount + 1); 
-        dp[0] = 0;
-
-        for(int i = 1; i <= amount; i++) {
-            for(int j = 0; j < n; j++) {
-                if(coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
-        }
-
-        return dp[amount] > amount ? -1 : dp[amount];
+        dp.resize(n, vector<int> (amount + 1, -1));
+        int res = coinChangeHelper(coins, n - 1, amount);
+        return res >= 1e9 ? -1 : res;
     }
 };
