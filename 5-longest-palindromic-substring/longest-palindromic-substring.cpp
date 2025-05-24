31c0c59f27be2184d1d12d5b6e597b53dc277d2f
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-24 13:03:13 +0530

    Time: 17 ms (60.26%) | Memory: 18.6 MB (49.30%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string expandFromCenter(string& s, int i, int j) {
-        while(i >= 0 && j < s.length() && s[i] == s[j]) {
+        while (i >= 0 && j < s.length() && s[i] == s[j]) {
             i--;
             j++;
         }
@@ -10,14 +10,17 @@ public:
     string longestPalindrome(string s) {
         int n = s.length();
         string maxSizeString = "";
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             string oddLenString = expandFromCenter(s, i, i);
-            string evenLenString = expandFromCenter(s, i, i + 1);
+            string evenLenString = "";
+            if (i + 1 < n) {
+                evenLenString = expandFromCenter(s, i, i + 1);
+            }
 
-            if(maxSizeString.length() < oddLenString.length()) {
+            if (maxSizeString.length() < oddLenString.length()) {
                 maxSizeString = oddLenString;
             }
-            if(maxSizeString.length() < evenLenString.length()) {
+            if (maxSizeString.length() < evenLenString.length()) {
                 maxSizeString = evenLenString;
             }
         }
