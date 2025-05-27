614c3473f05774b30926dde9f84d02f89d7b89c8
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-05-27 10:10:19 +0530

    Time: 3 ms (88.73%) | Memory: 8.6 MB (81.14%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,43 +1,19 @@
 class Solution {
 public:
     int n = 0;
-    int cnt = 0;
-    bool isPalindrome(string& ans) {
-        int i = 0, j = ans.length() - 1;
-        while(i < j) if(ans[i++] != ans[j--]) return false;
-        return true;
-    }
-    void countHelper(string& s, int i, string& ans) {
-        // base case
-        if(!ans.empty() && isPalindrome(ans)) {
-            cnt++;
+    int count = 0;
+    int expandAcc_lengthType(string& s, int i, int j) {
+        while(j < n && i >= 0 && s[i] == s[j]) {
+            count++, i--, j++;
         }
-
-        for(int choices = i; choices < n; choices++) {
-            // choose
-            ans.push_back(s[choices]);
-            // move forward
-            countHelper(s, i + 1, ans);
-            // backtrack
-            ans.pop_back();
-            break; 
-        }
+        return count;
     }
     int countSubstrings(string s) {
         n = s.length();
-        string ans = "";
-        for(int i = 0; i < n; i++) countHelper(s, i, ans);
-        return cnt;
+        for(int i = 0; i < n; i++) {
+            expandAcc_lengthType(s, i, i);
+            expandAcc_lengthType(s, i, i + 1);
+        }
+        return count;
     }
 };
-
-/*
-  a b c
-  a
-  a b -> Not a palindrome
-  a b c -> Not a palindrome
-  b 
-  b c -> Not a palindrome
-  c
-  Output => "a", "b", "b" = (?count) 3
-*/
