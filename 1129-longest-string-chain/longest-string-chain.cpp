4df8688ef706ccd81ec01b63ca594e7d66fa017a
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-06-27 07:11:40 +0530

    Time: 44 ms (63.37%) | Memory: 18.6 MB (85.97%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,34 +1,37 @@
 class Solution {
 public:
-    bool compareTheTwo(string& longer, string& shorter) {
+    bool isOneApart(string& longer, string& shorter) {
         if (longer.length() != shorter.length() + 1) return false;
         int i = 0, j = 0;
-        int len = longer.length();
-        while(i < len) {
-            if(shorter[j] == longer[i] && j < shorter.length()) {
-                i++, j++;
+        while (i < longer.length()) {
+            if (j < shorter.length() && longer[i] == shorter[j]) {
+                ++i;
+                ++j;
             } else {
-                i++;
+                ++i;
             }
         }
         return j == shorter.length();
     }
+
     int longestStrChain(vector<string>& words) {
         int n = words.size();
         sort(words.begin(), words.end(), [](const string& a, const string& b) {
-            return a.size() < b.size();
+            return a.length() < b.length();
         });
 
-        vector<int> dp(n, 1);
+        vector<int> dp(n, 1); 
         int ans = 1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (compareTheTwo(words[i], words[j]) && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    ans = max(ans, dp[i]);
+
+        for (int i = 0; i < n; ++i) {
+            for (int prev = 0; prev < i; ++prev) {
+                if (isOneApart(words[i], words[prev]) && dp[i] < dp[prev] + 1) {
+                    dp[i] = dp[prev] + 1;
                 }
             }
+            ans = max(ans, dp[i]);
         }
+
         return ans;
     }
 };
