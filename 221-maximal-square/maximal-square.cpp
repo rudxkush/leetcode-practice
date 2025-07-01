cfb3b9716f9b5650f736f3ac0cd4075f2ffba949
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-07-02 00:52:24 +0530

    Time: 8 ms (63.10%) | Memory: 30.9 MB (77.15%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -6,18 +6,14 @@ public:
         int m = matrix[0].size();
         vector<vector<int>> dp(n, vector<int>(m, 0));
         int maxSide = 0;
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (matrix[i][j] == '1') {
-
-                    if (i == 0 || j == 0)
+                    if (i == 0 || j == 0) 
                         dp[i][j] = 1;
-                    else
-                        dp[i][j] = 1 + min(
-                                           min(dp[i - 1][j], dp[i][j - 1]),
-                                           dp[i - 1][j - 1]
-                                          );
+                    else 
+                        dp[i][j] = 1 + min(dp[i - 1][j],
+                                           min(dp[i][j - 1], dp[i - 1][j - 1]));
                     maxSide = max(maxSide, dp[i][j]);
                 }
             }
