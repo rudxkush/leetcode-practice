fa6f790f5a7e70c361768c6e9f7eac87a39e52e9
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-07-08 18:42:45 +0530

    Time: 18 ms (57.40%) | Memory: 44.1 MB (30.54%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,16 +1,23 @@
-class MyCalendar {
-    set<pair<int, int>> st;
+#include <map>
+using namespace std;
 
+class MyCalendar {
+    map<int, int> mp; // start -> end
 public:
     MyCalendar() {}
 
     bool book(int startTime, int endTime) {
-        for (const auto& interval : st) {
-            if (max(interval.first, startTime) < min(interval.second, endTime)) {
-                return false;
-            }
+        auto next = mp.lower_bound(startTime);
+        
+        if(next != mp.end() && next->first < endTime) return false;
+
+        if(next != mp.begin()) {
+            auto prev_it = prev(next);
+            if(prev_it->second > startTime) return false;
         }
-        st.insert({startTime, endTime});
+
+        // No overlaps
+        mp[startTime] = endTime;
         return true;
     }
 };
