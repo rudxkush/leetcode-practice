2536a290602a1138aa1b2867a697695a254a0e99
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-08-01 11:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (70.35%) - LeetSync

--- 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -1,50 +1,59 @@
-class DisjointSet {
-    vector<int> parent, size;
-public:
-    // Initializes disjoint sets
-    DisjointSet(int n) {
-        parent.resize(n);
-        size.resize(n, 1);
-        for(int i = 0;i < n;i++) {
+class disjointSet {
+    vector<int> parent;
+    vector<int> size;
+public: 
+    disjointSet(int k) {
+        parent.resize(k);
+        size.resize(k, 1);
+        for(int i = 0; i < k; i++) {
             parent[i] = i;
         }
     }
-    // path compression
-    int findUltimateParent(int node) {
-        if(node == parent[node])
-            return node;
-            
-        return parent[node] = findUltimateParent(parent[node]);
+
+    int findUltimateParent(int child) {
+        if(child == parent[child]) return child;
+        return parent[child] = findUltimateParent(parent[child]); 
+        // parent[child] is for path compression **always remember from now on. 
     }
     
-    void unionBySize(int u, int v) {
-     int ultimateParent_u = findUltimateParent(u);
-     int ultimateParent_v = findUltimateParent(v);
-      
-     if(ultimateParent_u == ultimateParent_v) return ;
-      
-     if(ultimateParent_u < ultimateParent_v) {
-        parent[ultimateParent_v] = ultimateParent_u;
-        size[ultimateParent_u] += size[ultimateParent_v];
-     } else {
-        parent[ultimateParent_u] = ultimateParent_v;
-        size[ultimateParent_v] += size[ultimateParent_u];
-     }
-   }
+    void unionFind(int u, int v) {
+        int ultimateParent_u = findUltimateParent(u);
+        int ultimateParent_v = findUltimateParent(v);
+
+        if(ultimateParent_u == ultimateParent_v) return ;
+        if(ultimateParent_u > ultimateParent_v) {
+            parent[ultimateParent_u] = ultimateParent_v;
+            size[ultimateParent_v] += size[ultimateParent_u];
+        } else {
+            parent[ultimateParent_v] = ultimateParent_u;
+            size[ultimateParent_u] += size[ultimateParent_v];
+        }
+    }
 };
-class Solution {
+class Solution {    
 public:
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        DisjointSet ds(26);
-        for(int i = 0; i < s1.length(); i++) {
-            int u = s1[i] - 'a';
-            int v = s2[i] - 'a';
-            ds.unionBySize(u , v);
-        }
+        /*
+           given : s1, s2, baseStr
+           s1 = "parker"
+           s2 = "morris"
+           baseStr = "parser"
+           Union both the strings according to the numeric ASCII Value
+           and then for the answer part we can use the ultimateParentFind 
+           to get the smaller letter first.
+        */
+        int n = s1.length();
+        disjointSet obj(26);
+        for(int i = 0; i < n; ++i) 
+            obj.unionFind(s1[i] - 'a', s2[i] - 'a');
 
-        for(int i = 0; i < baseStr.length(); i++) {
-            baseStr[i] =  ds.findUltimateParent(baseStr[i] - 'a') + 'a';
-        }
-        return baseStr;
+        int m = baseStr.length();
+        string sortedString;
+        for(int i = 0; i < m; ++i) {
+            int letter = baseStr[i] - 'a';
+            int ultimateParent_letter = obj.findUltimateParent(letter);
+            sortedString += ultimateParent_letter + 'a';
+        }   
+        return sortedString;
     }
 };
