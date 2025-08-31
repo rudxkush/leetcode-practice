9f5795439d3d9f99070a86fa154f0d55a0a99304
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-08-31 18:05:06 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (8.05%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -1,29 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        if (root == nullptr)
-            return res;
+        vector<int> ans;
+        if(root == NULL) return ans;
+        unordered_map<int, int> mp; // row -> val
+        queue<pair<TreeNode*, int>> q;// node, row
+        int maxRow = 0;
 
-        queue<TreeNode*> TreeQueue;
-        TreeQueue.push(root);
+        q.push({root, 0});
+        while(!q.empty()) {
+            auto [node, row] = q.front(); q.pop();
+            maxRow = max(maxRow, row);
 
-        while (!TreeQueue.empty()) {
-            int size = TreeQueue.size();
-            for (int i = 0; i < size; i++) {
-                TreeNode* current = TreeQueue.front();
-                TreeQueue.pop();
+            if(mp.find(row) == mp.end()) mp[row] = node->val;
 
-                if (i == (size - 1))
-                    res.push_back(current->val);
+            if(node->right) 
+                q.push({node->right, row + 1}); 
 
-                if (current->left != NULL)
-                    TreeQueue.push(current->left);
+            if(node->left) 
+                q.push({node->left, row + 1}); 
 
-                if (current->right != NULL)
-                    TreeQueue.push(current->right);
-            }
         }
-        return res;
+        for (int i = 0; i <= maxRow; i++) {
+            ans.push_back(mp[i]);
+        }
+        return ans;
     }
 };
