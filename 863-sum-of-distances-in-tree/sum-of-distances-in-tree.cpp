0a6bee37b5a8e07e8a630a49fe85ccf01b01e3c5
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-09-25 05:05:54 +0530

    Time: 119 ms (33.60%) | Memory: 104.8 MB (46.27%) - LeetSync

--- 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
-    vector<int> dp, sbtrSize, ans;
-    void dfs1(vector<vector<int>>& adj, int node, int par) {
-        for (auto& child : adj[node]) {
-            if (child == par)
-                continue;
-            dfs1(adj, child, node);
-            sbtrSize[node] += sbtrSize[child];
-            dp[node] += (dp[child] + sbtrSize[child]);
+    int totalNodes = 0;
+    void dfs1(vector<vector<int>>& tree, vector<int>& noOfNodes, int node, int parent) {
+        for(auto nei : tree[node]) {
+            if(nei == parent) continue;
+            dfs1(tree, noOfNodes, nei, node);
+            noOfNodes[node] += noOfNodes[nei];
         }
+        noOfNodes[node] += 1;
     }
-    void dfs2(vector<vector<int>>& adj, int node, int par, int n, int val) {
-        ans[node] = val;
-        for (auto& child : adj[node]) {
-            if (child == par)
-                continue;
-            int newval = val + n - 2 * sbtrSize[child];
-            dfs2(adj, child, node, n, newval);
+    int dfs2(vector<vector<int>>& tree, int node, int parent, int depth) {
+        int count = depth;
+        for(auto nei : tree[node]) {
+            if(nei == parent) continue;     
+            count += dfs2(tree, nei, node, depth+1);
+        }
+        return count;
+    }
+    void dfs3(vector<vector<int>>& tree, vector<int>& ans, 
+              vector<int>& noOfNodes, int node, int parent) {
+        for(auto nei : tree[node]) {
+            if(nei == parent) continue;
+            ans[nei] = ans[node] + (totalNodes - noOfNodes[nei]) - noOfNodes[nei];
+            dfs3(tree, ans, noOfNodes, nei, node);
         }
     }
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0], v = edges[i][1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+        // Building Tree here!
+        vector<vector<int>> tree(n);
+        for (auto edge : edges) {
+            tree[edge[0]].push_back(edge[1]);
+            tree[edge[1]].push_back(edge[0]);
         }
-        dp.resize(n, 0);
-        sbtrSize.resize(n, 1);
-        ans.resize(n, 1);
-        dfs1(adj, 0, -1);
-        dfs2(adj, 0, -1, n, dp[0]);
+        vector<int> ans(n, 0);
+        vector<int> noOfNodes(n, 0);
+
+        // For getting the size of every subtree in the tree.
+        dfs1(tree, noOfNodes, 0,-1);
+        // For getting the ans for the root. 
+        // Assuming root as my parent.
+        ans[0] = dfs2(tree, 0, -1, 0); 
+        // For getting the ans for the rest of the tree.
+        totalNodes = n;
+        dfs3(tree, ans, noOfNodes, 0, -1); 
         return ans;
     }
 };
