1acf6d5a22504c5a633aebe1b2527e7088385df4
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-08 02:32:31 +0530

    Time: 3 ms (69.69%) | Memory: 27.6 MB (16.50%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,33 +1,45 @@
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        unordered_map<int, int> inMap;
-        for (int i = 0; i < inorder.size(); i++) {
-            inMap[inorder[i]] = i;
-        }
-
-        return buildTreeHelper(postorder, 0, postorder.size() - 1,
-                               inorder, 0, inorder.size() - 1,
-                               inMap);
-    }
-    TreeNode* buildTreeHelper(vector<int>& postorder, int postStart, int postEnd,
-                              vector<int>& inorder, int inStart, int inEnd,
-                              unordered_map<int, int>& inMap) {
-                                
-        if(postStart > postEnd || inStart > inEnd) return NULL;
+    TreeNode* recBuild(vector<int>& inorder, vector<int>& postorder,
+                       unordered_map<int, int>& inMap, int postStart,
+                       int postEnd, int inStart, int inEnd) {
+        if (postEnd < postStart || inEnd < inStart)
+            return nullptr;
 
         TreeNode* root = new TreeNode(postorder[postEnd]);
-        int inRoot = inMap[root->val];
-        int numsLeft = inRoot - inStart;
 
-        root->left = buildTreeHelper(postorder, postStart, postStart + numsLeft - 1,
-                                     inorder, inStart, inRoot - 1,
-                                     inMap);
+        int inMapRightLen = inEnd - inMap[postorder[postEnd]];
+        int rootIndex = inMap[postorder[postEnd]];
 
-        root->right =  buildTreeHelper(postorder, postStart + numsLeft, postEnd - 1,
-                                       inorder, inRoot + 1, inEnd,
-                                       inMap);
+        root->right =
+            recBuild(inorder, postorder, inMap, postEnd - inMapRightLen,
+                     postEnd - 1, rootIndex + 1, inEnd);
+
+        root->left =
+            recBuild(inorder, postorder, inMap, postStart,
+                     postEnd - inMapRightLen - 1, inStart, rootIndex - 1);
 
         return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        /*                Approachin the approach!
+                    postorder: [9,15,7,20,3] l->r->root
+                    Inorder:   [9,3,15,20,7] l->root->r
+                    inMap:     [0,1, 2, 3,4]
+                          postorder[postStart] = node
+                        /                             \
+                Postorder Range                   Postorder Range
+                [1, 2, 3]                         [0, 0]
+                Inorder Range.                    Inorder range
+                [0 0]                             [2, 3, 4]
+        */
+        unordered_map<int, int> inMap;
+        int n = postorder.size();
+        for (int i = 0; i < n; i++) {
+            inMap[inorder[i]] = i;
+        }
+        TreeNode* temp =
+            recBuild(inorder, postorder, inMap, 0, n - 1, 0, n - 1);
+        return temp != NULL ? temp : NULL;
     }
 };
