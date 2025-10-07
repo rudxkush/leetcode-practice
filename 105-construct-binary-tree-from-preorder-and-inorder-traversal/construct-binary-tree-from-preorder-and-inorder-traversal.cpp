088ab01969c55db83200a48533021a207e6072f0
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-08 02:09:09 +0530

    Time: 4 ms (57.73%) | Memory: 27.5 MB (53.67%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,45 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
+    TreeNode* recBuild(vector<int>& preorder, vector<int>& inorder,
+                       unordered_map<int, int>& inMap, int preStart, int preEnd,
+                       int inStart, int inEnd) {
+        if (preEnd < preStart || inEnd < inStart)
+            return nullptr;
+
+        TreeNode * root = new TreeNode(preorder[preStart]);
+
+        int inMapLeftlen = inMap[preorder[preStart]] - inStart;
+        int rootIndex = inMap[preorder[preStart]];
+
+        root->left = recBuild(preorder, inorder, inMap, preStart + 1,
+                         preStart + inMapLeftlen, inStart, rootIndex - 1);
+
+        root->right = recBuild(preorder, inorder, inMap, preStart + inMapLeftlen + 1,
+                          preEnd, rootIndex + 1, inEnd);
+
+        return root;
+    }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        /*                Approachin the approach!
+                    preorder: [3,9,20,15,7] root->l->r
+                    Inorder:  [9,3,15,20,7] l->root->r
+                    inMap:    [0,1, 2, 3,4]
+                           preorder[preStart] = node
+                        /                             \
+                preOrder Range                    Preorder Range
+                [1, 1]                            [2, 3, 4]
+                Inorder Range.                    Inorder range
+                [0 0]                             [2, 3, 4]
+        */
         unordered_map<int, int> inMap;
-        for (int i = 0; i < inorder.size(); i++) {
+        int n = preorder.size();
+        for (int i = 0; i < n; i++) {
             inMap[inorder[i]] = i;
         }
-
-        return buildTreeHelper(preorder, 0, preorder.size() - 1, 
-                               inorder, 0, inorder.size() - 1, 
-                               inMap);
-    }
-    TreeNode* buildTreeHelper(vector<int>& preorder, int preStart, int preEnd,
-                              vector<int>& inorder, int inStart, int inEnd,
-                              unordered_map<int, int>& inMap) {
-                                
-        if (preStart > preEnd || inStart > inEnd)
-            return NULL;
-
-        TreeNode* root = new TreeNode(preorder[preStart]);
-        int inRoot = inMap[root->val];
-        int numsleft = inRoot - inStart;
-
-        root->left = buildTreeHelper(preorder, preStart + 1, preStart + numsleft, 
-                                     inorder, inStart, inRoot - 1, 
-                                     inMap);
-
-        root->right = buildTreeHelper(preorder, preStart + numsleft + 1, preEnd, 
-                                      inorder, inRoot + 1, inEnd, 
-                                      inMap);
-        return root;
+        TreeNode* temp = recBuild(preorder, inorder, inMap, 0, n - 1, 0, n - 1);
+        return temp != NULL ? temp : NULL;
     }
 };
