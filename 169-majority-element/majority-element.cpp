aa581648a599fbb3c863d186edd0077b80bebb87
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-09 23:32:24 +0530

    Time: 3 ms (29.79%) | Memory: 28.2 MB (28.66%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int candidate = nums[0];
-        int freq = 1;
+        int candidate = 0;
+        int freq = 0;
         int n = nums.size();
         for(int i = 0; i < n; i++) {
-            if(candidate == nums[i]) {
-                freq++;
+            if(freq == 0) {
                 candidate = nums[i];
+                freq = 1;
             } else {
-                freq--;
-                if(freq == 0) {
-                    candidate = nums[i];
-                    freq = 1;
+                if(nums[i] == candidate) {
+                    freq++;
+                } else {
+                    freq--;
                 }
             }
         }
-        return candidate;
+        // validate that candidate has appeared > n/2 times
+        int hadAppeared = 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] == candidate) {
+                hadAppeared += 1;
+            }
+        } 
+        return hadAppeared > n/2 ? candidate : -1;
     }
 };
-// 3, 2, 3
-// candidate = 3
-// freq = 1
-
-// i = 0, freq = 1, candidate = 3
-// i = 1, freq = 0, candidate = 2
-// i = 2 , freq = 
