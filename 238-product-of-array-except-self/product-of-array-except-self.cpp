51e9e789b9a24b09b5d9c15501325a9592ab782c
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-11 04:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.20%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,24 +2,28 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefixProduct(n, 1);
-        vector<int> ans(n, 1);
-        for(int i = 1; i < n; i++) {
-            prefixProduct[i] = prefixProduct[i - 1] * nums[i - 1];
+        vector<int> result(n, 0);
+        int totalProduct = 1;
+        unordered_set<int> st; // collect all the indices if any present!
+        bool zeroPrs = false;  // zero -> present or not
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != 0) {
+                totalProduct *= nums[i];
+            } else {
+                zeroPrs = true;
+                st.insert(i);
+            }
         }
-        int suffixProduct = 1;
-        for(int i = n - 1; i >= 0; i--) {
-            ans[i] = prefixProduct[i] * suffixProduct;
-            suffixProduct *= nums[i]; 
+        if (st.size() > 1) {
+            return vector<int>(n, 0);
+        } else if (zeroPrs) {
+            result[*st.begin()] = totalProduct;
+        } else {
+            for (int i = 0; i < n; i++) {
+                int productAtIndexI = totalProduct / nums[i];
+                result[i] = productAtIndexI;
+            }
         }
-        return ans;
+        return result;
     }
 };
-
-/*
-    nums =         [1, 2, 3, 4]
-    prefixProduct: [1, 1, 1, 1]
-    ans:           [?, ?, ?, ?]
-    prefixProduct: [1,  1,  2, 6]
-    ans:           [24, 12, 8, 6]
-*/
