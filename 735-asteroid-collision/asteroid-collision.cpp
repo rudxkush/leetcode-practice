d3c70232399e27ffb95933b478c0893f770ce1d3
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-18 00:07:38 +0530

    Time: 4 ms (15.69%) | Memory: 22 MB (59.85%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> stack;
-
-        for (int asteroid : asteroids) {
-            bool alive = true;
-            while (!stack.empty() && asteroid < 0 && stack.top() > 0) {
-                if (stack.top() < -asteroid) {
-                    stack.pop();
+    vector<int> asteroidCollision(vector<int>& ast) {
+        int n = ast.size();
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            bool isAlive = true; // assuming it doesn't collide
+            while(!st.empty() && st.top() > 0 && ast[i] < 0) {
+                if(-ast[i] > st.top()) {
+                    st.pop();
                     continue;
-                } else if (stack.top() == -asteroid) {
-                    stack.pop();
+                } else if(-ast[i] == st.top()){
+                    st.pop();
                 }
-                alive = false;
+                isAlive = false;
                 break;
             }
-            if (alive) {
-                stack.push(asteroid);
+            if(isAlive) {
+                st.push(ast[i]);
             }
         }
-
-        vector<int> res(stack.size());
-        for (int i = res.size() - 1; i >= 0; i--) {
-            res[i] = stack.top();
-            stack.pop();
+        int survived = st.size() - 1;
+        vector<int> astW(survived + 1);
+        while (!st.empty()) {
+            astW[survived] = st.top();
+            survived--;
+            st.pop();
         }
-        return res;
+        return astW;
     }
 };
+/*
+        Think of all the edge cases
+    + -> Right          - -> Left
+    1. Right collides with Left
+        - (Right.val < Left.val) { gets destroyed and next right is faced}
+        - (Right.val == Left.val) { gets completely destroyed along with right}
+        - (Right.val > Left.val) { doesn't get destroyed }
+    2. No Right and you get a Left
+        - skip!!
+*/
