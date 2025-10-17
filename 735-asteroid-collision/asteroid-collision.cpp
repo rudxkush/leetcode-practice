96c738a9c84834121c717a5644dae68c8e7d1942
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-18 00:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (18.07%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -4,19 +4,17 @@ public:
         int n = ast.size();
         stack<int> st;
         for (int i = 0; i < n; i++) {
-            bool isAlive = true; // assuming it doesn't collide
-            while(!st.empty() && st.top() > 0 && ast[i] < 0) {
-                if(-ast[i] > st.top()) {
-                    st.pop();
-                    continue;
-                } else if(-ast[i] == st.top()){
+            if(ast[i] > 0) st.push(ast[i]);
+            else {
+                int left = -ast[i];
+                while(!st.empty() && st.top() < left && st.top() > 0) {
                     st.pop();
                 }
-                isAlive = false;
-                break;
-            }
-            if(isAlive) {
-                st.push(ast[i]);
+                if(!st.empty() && st.top() == left) {
+                    st.pop();
+                } else if(st.empty() || st.top() < 0) {
+                    st.push(ast[i]);
+                }
             }
         }
         int survived = st.size() - 1;
@@ -29,13 +27,3 @@ public:
         return astW;
     }
 };
-/*
-        Think of all the edge cases
-    + -> Right          - -> Left
-    1. Right collides with Left
-        - (Right.val < Left.val) { gets destroyed and next right is faced}
-        - (Right.val == Left.val) { gets completely destroyed along with right}
-        - (Right.val > Left.val) { doesn't get destroyed }
-    2. No Right and you get a Left
-        - skip!!
-*/
