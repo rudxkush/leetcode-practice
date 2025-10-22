6dfe9f295572fa5d622079cc8621aabd20bfc097
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-10-22 07:15:55 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (44.99%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,24 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int l = 0, r = n - 1, leftMax = 0, rightMax = 0, total = 0;
-        while (l < r) {
-            if (height[l] <= height[r]) {
-                if (leftMax > height[l]) {
-                    total += leftMax - height[l];
-                } else {
-                    leftMax = height[l];
-                }
-                l++;
-            } else {
-                if (rightMax > height[r]) {
-                    total += rightMax - height[r];
-                } else {
-                    rightMax = height[r];
-                }
-                r--;
-            }
+        // I would just store gR **prefix**
+        vector<int> gR(n);
+        gR[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            gR[i] = max(gR[i + 1], height[i]);
         }
-        return total;
+        int sum = 0;
+        int gL = height[0];
+        for (int i = 0; i < n; i++) {
+            int minHeight = min(gL, gR[i]);
+            if (minHeight > height[i])
+                sum += minHeight - height[i];
+            // update gL
+            gL = max(gL, height[i]);
+        }
+        return sum;
     }
 };
