3403b1fd42e534611952655a4966ddc6f669194b
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-11-28 22:25:01 +0530

    Time: 3 ms (73.44%) | Memory: 18.6 MB (10.76%) - LeetSync

---
+++ 974-reorder-data-in-log-files/reorder-data-in-log-files.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        vector<string> digitLogs, letterLogs, ans;
+        // separate the two different type of logs
+        for(auto log : logs) {
+            int index = log.find(" "); // find the first space delimiter
+            if(isdigit(log[index + 1])) {
+                digitLogs.push_back(log);
+            } else {
+                letterLogs.push_back(log);
+            }
+        }
+
+        // sort both the logs separately
+        // sort(letter logs)
+        sort(letterLogs.begin(), letterLogs.end(), 
+        [](const string& A, const string& B) {
+            int indexA = A.find(" ");
+            int indexB = B.find(" ");
+            if(A.substr(indexA + 1) == B.substr(indexB + 1)) {
+                return A.substr(0, indexA) < B.substr(0, indexB);
+            } else {
+                return A.substr(indexA + 1) < B.substr(indexB + 1);
+            }
+        }); 
+
+        // ans = sort(letterLogs) + digitLogs
+        ans.insert(ans.begin(), letterLogs.begin(), letterLogs.end());
+        ans.insert(ans.end(), digitLogs.begin(), digitLogs.end());
+        return ans;
+    }
+};
