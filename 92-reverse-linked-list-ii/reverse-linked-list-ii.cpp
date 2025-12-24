1be30904348d621fab550fda24e6c30bf92d6e8a
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2025-12-24 15:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (70.86%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -10,23 +10,44 @@
  */
 class Solution {
 public:
+    ListNode* reverse(ListNode* start, ListNode* end) {
+        ListNode* curr = start;
+        ListNode* prev = NULL;
+        ListNode* stop = end->next;
+        while(curr != stop) {
+            ListNode* nextNodes = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNodes;
+        }
+        return prev;
+    }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
-        ListNode* curr = head;
-        ListNode* prev = dummy;
-
-        for (int i = 1; i < left; i++) {
+        ListNode* curr = dummy;
+        for(int i = 1; i < left; i++) {
+            // Avoids NULL references on the right of the LL
+            if(curr->next == NULL && i < left) return dummy->next;
             curr = curr->next;
-            prev = prev->next;
+        }
+        int winSize = right - left;
+        ListNode* end = curr->next;
+        for(int i = 0; i < winSize; i++) {
+            // Avoids NULL references on the left of the LL
+            if(end->next == NULL && i < right) {
+                break;
+            }
+            end = end->next;
         }
 
-        for(int i = 0; i < (right - left); i++) {
-            ListNode* nextNode = curr->next;
-            curr->next = nextNode->next;
-            nextNode->next = prev->next;
-            prev->next = nextNode;
-        }
+        ListNode* revHead = curr->next;
+        ListNode* prev = curr;
+        ListNode* nextNodes = end->next;
+        ListNode* revLL = reverse(revHead, end);
+        prev->next = revLL;
+        revHead->next = nextNodes;
+        
         return dummy->next;
     }
 };
