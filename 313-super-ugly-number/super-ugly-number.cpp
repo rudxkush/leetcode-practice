c0cae97e05c297e7ab5355809d1328fd878067f0
Rudra Pratap Singh Kushwah <112988464+rudxkush@users.noreply.github.com>
2026-01-09 17:42:10 +0530

    Time: 45 ms (87.19%) | Memory: 15.7 MB (14.34%) - LeetSync

--- 313-super-ugly-number/super-ugly-number.cpp
+++ 313-super-ugly-number/super-ugly-number.cpp
@@ -1,29 +1,39 @@
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        if(n == 1) {
-            return n;
-        }
-        vector<long long> dp = {1};
-        long long a = primes.size();
-        vector<long long> multiply(primes.size(), 0);
+        int num_of_primes = primes.size();
 
-        int i = 2;
-        while(i <= n){
-            long long mini = INT_MAX;
-            for(int j = 0; j < a; j++){
-                long long a = dp[multiply[j]];
-                mini = min(mini, (a * primes[j]));
+        vector<int> trackIncrementsOfPrimes(num_of_primes, 0);
+        vector<long long> currValues(num_of_primes);
+        vector<long long> ans;
+
+        ans.push_back(1);
+
+        for (int i = 0; i < num_of_primes; i++) {
+            currValues[i] = primes[i];
+        }
+
+        long long nextNum = 1;
+
+        for (int i = 1; i < n; i++) {
+            long long mini = LLONG_MAX;
+
+            for (long long num : currValues) {
+                mini = min(mini, num);
             }
-            
-            dp.push_back(mini);
-            for(int j = 0; j < a; j++){
-                if(mini % primes[j] == 0){
-                    multiply[j]++;
+
+            ans.push_back(mini);
+            nextNum = mini;
+
+            for (int j = 0; j < num_of_primes; j++) {
+                if (mini == currValues[j]) {
+                    trackIncrementsOfPrimes[j]++;
+                    currValues[j] =
+                        ans[trackIncrementsOfPrimes[j]] * primes[j];
                 }
             }
-            i++;
         }
-        return dp[n - 1];
+
+        return (int)nextNum;
     }
 };
